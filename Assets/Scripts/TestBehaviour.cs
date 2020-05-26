bcf8bb74240ff3cceec80e603c643609121d6fd2
David Orbelian <david.orbelian@gmail.com>
2020-05-27 00:54:08 +0400

    Add TestBehaviour script.

---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using Gamgaroo.MVVM.Runtime.Extensions;
+using Gamgaroo.MVVM.Runtime.Views.Primitives;
+using Gamgaroo.Reactive.Runtime;
+using Gamgaroo.Reactive.Runtime.Abstractions;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    ///     This is a View binding sample. After the View is bound to the Reactive Property,
+    ///     it will update automatically when the Property value is updated.
+    /// </summary>
+    public sealed class TestBehaviour : MonoBehaviour
+    {
+        private readonly IReactiveProperty<float> _input = new ReactiveProperty<float>();
+
+        private void Start()
+        {
+            // Bind the View to the Property
+            GetComponent<TextView>().Bind(_input);
+        }
+
+        private void Update()
+        {
+            // Update the Property value
+            _input.Value += Time.deltaTime;
+        }
+    }
+}
